7f54c46dda7e194a7511cbadaa5cd572801961d3
Eduardo <eduardo.souza01@outlook.com>
2023-05-10 20:22:36 -0300

    10/05

--- assunto7_ListasEncadeadasGenericas/src/listas/ListaIntCrescente.java
+++ assunto7_ListasEncadeadasGenericas/src/listas/ListaIntCrescente.java
@@ -10,6 +10,7 @@ public class ListaIntCrescente {
 	private NO lista = null;
 
 	public void insere(int elem) {
+		// alocar espaco para o NO
 		NO novo = new NO();
 		novo.dado = elem;
 		if (lista == null) {
@@ -19,32 +20,56 @@ public class ListaIntCrescente {
 			if (novo.dado < lista.dado) {
 				novo.prox = lista;
 				lista = novo;
-			}
-			else {
+			} else {
+				// comeca referenciando o mesmo NO que lista
 				NO aux = lista;
 				boolean achou = false;
-			// ou (achou == false)
-				while(aux.prox!=null && !achou) {
-					if(aux.prox.dado < novo.dado) {
-						aux= aux.prox;
-					}else {
-						achou=true;
+				// ou (achou == false)
+				while (aux.prox != null && !achou) {
+					if (aux.prox.dado < novo.dado) {
+						aux = aux.prox;
+					} else {
+						achou = true;
 					}
 				}
 				novo.prox = aux.prox;
 				aux.prox = novo;
-				
+
 			}
 		}
 	}
+
+	public boolean remove(int valor) {
+		boolean achou = false;
+		if (lista != null) {
+			if (valor == lista.dado) {
+				lista = lista.prox;
+			} else {
+				NO aux = lista;
+
+				while (aux.prox != null && !achou) {
+					if (aux.prox.dado != valor) {
+						aux = aux.prox;
+					} else {
+						achou = true;
+						// pega o sucessor do NO que vai ser retirado
+						aux.prox = aux.prox.prox;
+					}
+
+				}
+			}
+		}
+		return achou;
+	}
+
 	public void apresenta() {
 		NO aux = lista;
 		System.out.println("\n *** Lista ***");
-		while(aux!= null) {
+		while (aux != null) {
 			System.out.println("\t" + aux.dado);
-			aux= aux.prox;
+			aux = aux.prox;
 		}
-		
+
 	}
 
 }
--- assunto7_ListasEncadeadasGenericas/src/teste/MainTeste.java
+++ assunto7_ListasEncadeadasGenericas/src/teste/MainTeste.java
@@ -7,20 +7,37 @@ import listas.ListaIntCrescente;
 public class MainTeste {
 
 	public static void main(String[] args) {
-		
+
 		Scanner teclado = new Scanner(System.in);
 		ListaIntCrescente lista = new ListaIntCrescente();
-		
+
 		System.out.print("Informe um valor positivo para inserir ou negativo para encerrar: ");
 		int valor = teclado.nextInt();
-		while(valor>=0) {
+		while (valor >= 0) {
 			lista.insere(valor);
 			lista.apresenta();
 			System.out.println("Informe um valor positivo para inserir ou negativo para encerrar: ");
 			valor = teclado.nextInt();
-			
+
 		}
-		
+
+		System.out.print("\n\nInforme um valor positivo para remover ou negativo para encerrar: ");
+		valor = teclado.nextInt();
+		while (valor >= 0) {
+			if (!lista.remove(valor)) {
+				System.out.println("Valor nao encontrado na lista ");
+
+			} else {
+				lista.apresenta();
+			}
+
+			lista.remove(valor);
+			lista.apresenta();
+			System.out.println("Informe um valor positivo para remover ou negativo para encerrar: ");
+			valor = teclado.nextInt();
+
+		}
+
 		teclado.close();
 	}
 
